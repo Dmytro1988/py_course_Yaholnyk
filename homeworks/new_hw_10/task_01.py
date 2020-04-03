4fdc813a80f4aa55e568dfb78ed36f25f3939a49
Dmytro Yaholnyk <Yaholnyk88@gmail.com>
2020-04-04 00:30:06 +0300

    Homework 10 complecated

--- homeworks/new_hw_10/task_01.py
+++ homeworks/new_hw_10/task_01.py
@@ -1,11 +1,11 @@
 family = {
-'gradpa':    ('Alex', 76),
-'grandma':   ('Nona', 74),
-'dad':       ('Greg', 48),
-'mom':       ('July', 45),
-'son_older': ('Bob', 18),
-'son_middle':('Alex', 15),
-'son_young' :('Mark', 10)
+  'gradpa': ('Alex', 76),
+  'grandma': ('Nona', 74),
+  'dad': ('Greg', 48),
+  'mom': ('July', 45),
+  'son_older':('Bob', 18),
+  'son_middle':('Alex', 15),
+  'son_young' :('Mark', 10)
 }
 lst_age = []
 for values in family.values():
